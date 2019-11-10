13d3635ba5cf1013aa92a89d2c2dcb812a598453
Manivannan Ramasamy <manivannan.belfazt@gmail.com>
2019-11-10 16:56:42 +0530

    front end

---
+++ src/main/resources/static/precache-manifest.ce8224e2a4a309a2839ea4f48a62677f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "066f272f833f300b33b4419a47c4ce44",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aa4a364c7b58cc66dcd1",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "661200416ed66d580a77",
+    "url": "/static/css/main.1ebc2ee3.chunk.css"
+  },
+  {
+    "revision": "aa4a364c7b58cc66dcd1",
+    "url": "/static/js/2.25d32c70.chunk.js"
+  },
+  {
+    "revision": "661200416ed66d580a77",
+    "url": "/static/js/main.2348a1f9.chunk.js"
+  },
+  {
+    "revision": "46baedd75c44088e6c90",
+    "url": "/static/js/runtime-main.4a27514d.js"
+  }
+]);
