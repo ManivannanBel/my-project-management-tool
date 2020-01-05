2c4eafa3db070a46a1c8aa5bbbd42bbd6897a401
Manivannan Ramasamy <manivannan.belfazt@gmail.com>
2020-01-05 19:49:29 +0530

    build 2

--- src/main/resources/static/service-worker.js
+++ src/main/resources/static/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3383a8baf197575e767fc29de67269f4.js"
+  "/precache-manifest.547c634e3521dd98a908ad5de0ebac8e.js"
 );
 
 self.addEventListener('message', (event) => {
